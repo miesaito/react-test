7e58f204383d59777208341e094ea026b8064676
nishisuke <nishisuke13+git@gmail.com>
2018-03-31 15:47:50 +0900

    Add sample

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const World = () => {
+    return (
+        <h1>aufdsafksafdsaf</h1>
+    );
+}
+
+render(
+    <div>
+        <World />
+    </div>,
+    document.getElementById('app')
+);
